54a0c93a82eb41a4028f39238d070b2ba94f4050
Jean-Francois Arbour <jf.arbour@gmail.com>
2022-08-10 12:44:36 -0400

    Command line arguments

--- main.cpp
+++ main.cpp
@@ -1,11 +1,36 @@
+#include "viewer.h"
 #include "samegame.h"
 
 #include <iostream>
+#include <string>
+#include <fstream>
 
 using namespace std;
 
+
+constexpr size_t WIDTH = 15;
+constexpr size_t HEIGHT = 15;
+
+
 int main(int argc, char *argv[]) {
 
+    if (argc < 2) {
+        cerr << "USAGE: " << argv[0] << " INPUT FILE" << endl;
+        return EXIT_FAILURE;
+    }
 
-    return 0;
+    std::string input_fn = DATA_DIR + std::string(argv[1]);
+
+    ifstream ifs{ input_fn };
+    if (not ifs) {
+        cerr << "Failed to open input file " << input_fn << endl;
+        return EXIT_FAILURE;
+    }
+
+    SameGame sg{ WIDTH, HEIGHT };
+    sg.load(ifs);
+
+    Viewer::print(cout, sg);
+
+    return EXIT_SUCCESS;
 }
